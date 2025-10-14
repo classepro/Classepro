3626a64cb023dfb7732a72e9eb7d13e62d7298ca
classepro <classepro411@gmail.com>
2025-10-14 17:00:18 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'classepro-v1.10';
+const CACHE_NAME = 'classepro-v1.11';
 const urlsToCache = [
   'https://classepro.github.io/Classepro/',
   'https://classepro.github.io/Classepro/index.html',
